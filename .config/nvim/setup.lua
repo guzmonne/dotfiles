5ef5539830eddfd3b71106ebdd5ba02df38a138c
Guzmán Monné <guzmonne@hotmail.com>
2021-11-11 23:30:29 -0300

    feat: add lua plugins setup configuration

---
+++ .config/nvim/setup.lua
@@ -0,0 +1,14 @@
+-- Harpoon Setup --
+require("harpoon").setup({
+  global_settings = {
+    save_on_toggle = true,
+    save_on_change = true,
+    enter_on_sendcmd = true,
+  },
+})
+
+-- GitSigns Setup --
+require('gitsigns').setup({
+  numhl = true,
+  linehl = true,
+})
