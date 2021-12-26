7fa4e4d627b55ddb68aea00a82eb05b510267445
Guzmán Monné <guzmonne@hotmail.com>
2021-12-25 23:24:25 -0300

    feat: update toggle to support multiple maximized windows

--- config/nvim/plugins/toggle/lua/toggle.lua
+++ config/nvim/plugins/toggle/lua/toggle.lua
@@ -1,22 +1,30 @@
 Cache = {maximized_window_id = nil, origin_window_id = nil}
 
+-- If there is more than one window we want to maximize the
+-- window were we currently are. If there is only one window
+-- then:
+--   - if the window corresponds to a window we previously
+--     maximized, then we close it and go back to the window
+--     from which we arrived there.
+--   - else, if the window doesn't correspond to a window
+--     we previously maximized, then we do nothing.
+local Map = {}
+
 local function toggle_window()
     if vim.fn.winnr('$') > 1 then
         -- There is more than one window on this tab.
-        if Cache.maximized_window_id ~= nil then
-            vim.fn.win_gotoid(Cache.maximized_window_id)
-        else
-            Cache.origin_window_id = vim.fn.win_getid()
-            vim.cmd('tab sp')
-            Cache.maximized_window_id = vim.fn.win_getid()
-        end
+        local origin_window_id = vim.fn.win_getid()
+        vim.cmd('tab sp')
+        local maximized_window_id = vim.fn.win_getid()
+        Map[maximized_window_id] = origin_window_id
     else
-        -- There is only onw window on this tab.
-        if Cache.origin_window_id ~= nil then
+        -- There is only one window
+        local maximized_window_id = vim.fn.win_getid()
+        local origin_window_id = Map[maximized_window_id]
+        if origin_window_id ~= nil then
             vim.cmd('tabclose')
-            vim.fn.win_gotoid(Cache.origin_window_id)
-            Cache.maximized_window_id = nil
-            Cache.origin_window_id = nil
+            vim.fn.win_gotoid(origin_window_id)
+            Map[maximized_window_id] = nil
         end
     end
 end
