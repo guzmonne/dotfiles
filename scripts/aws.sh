8c02f4f5302be70307575a2f9ff447873366040c
Guzmán Monné <guzmonne@hotmail.com>
2022-05-25 10:41:17 -0300

    feat: update aws.sh script

--- scripts/aws.sh
+++ scripts/aws.sh
@@ -29,6 +29,65 @@ ssm() {
   aws ssm start-session --target "$instance_id"
 }
 
+# @cmd    Regenerates the SSO profiles.
+refresh() {
+  if [[ -z getawskeys ]]; then
+    echo "You neet to install getawskeys to continue"
+    exit 1
+  fi
+
+  getawskeys
+}
+
+# @cmd    Lists or creates a new secret
+# @flag -d --describe Print the full secrets description.
+# @arg name Secret name to fetch
+# @arg value Secret value
+# @arg description Secret description
+secrets() {
+  if [[ -n "$argc_name" ]]; then
+    # Working over a single secret
+    if [[ -n "$argc_value" ]]; then
+      # Creating a new secret
+      aws secretsmanager create-secret \
+        --name "$argc_name" \
+        --secret-string "$argc_value" \
+        $(if [[ -n "$argc_description" ]]; then echo "--description '$argc_description'"; else echo ""; fi)
+      exit 0
+    fi
+    # Reading
+    if [[ -n "$argc_describe" ]]; then
+      # Reading a secrets metadata
+      aws secretsmanager describe-secret --secret-id "$argc_name"
+    else
+      # Reading a secrets value
+      result=$(aws secretsmanager get-secret-value --secret-id "$argc_name")
+      if [[ -z "$result" ]]; then
+        echo "Secret not found" 2> /dev/stderr
+        exit 1
+      fi
+      secret=$(jq -r ".SecretString" <<<"$result")
+      if [[ -z "$secret" ]]; then
+        secret=$(jq -r ".SecretBinary" <<<"$result")
+      fi
+      echo "$secret"
+    fi
+    exit 0
+  fi
+
+  # Working over all secrets
+  if [[ -n $argc_describe ]]; then
+    aws secretsmanager list-secrets
+  else
+    aws secretsmanager list-secrets --query='SecretList[].Name' | jq -r '.[]'
+  fi
+}
+
+# @cmd    Connects to an instance through SSM identified by its `Name` tag.
+#         If more than one instance is found with that name a menu will be
+#         shown to the user to select the appropriate one.
+# @arg    name! Value of the `Name` tag for the instance
+
 # Run argc
 eval "$(argc "$0" "$@")"
 
