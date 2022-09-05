3acb6f7def6306a82d00d506adb118635a1fd7d5
Guzmán Monné <guzmonne@hotmail.com>
2022-09-04 23:48:16 -0300

    feat: fixed transparent backgrounds for UI elements

--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -4,5 +4,5 @@ vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keywords = false
 vim.g.tokyonight_italic_functions = false
 vim.g.tokyonight_transparent = true
+vim.g.tokyonight_dark_float = false
 vim.cmd [[colorscheme tokyonight ]]
-
