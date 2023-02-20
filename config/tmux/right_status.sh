2175b5642e5607dcf03b3bf60ba4a56bdf65de65
Guzmán Monné <guzmonne@hotmail.com>
2023-02-19 22:34:16 -0300

    feat: update tmux status bar colors

--- config/tmux/right_status.sh
+++ config/tmux/right_status.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 function tmux_session() {
-	tmux display-message -p '#S' | sed "s|$HOME|~|" | sed 's#~/Projects/##' | sed 's#/branches/#  #'
+  tmux display-message -p '#S' | sed "s|$HOME|~|" | sed 's#~/Projects/##' | sed 's#/branches/#  #'
 }
 
 function main() {
-	echo -e "#[bg=colour214,fg=black] $(tmux_session) 🔸🔷🔸"
+  echo -e "#[fg=colour214,bg=#1a1b26] $(tmux_session) 🔸🔷🔸"
 }
 
 main
