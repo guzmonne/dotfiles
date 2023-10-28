374e5664885191f3d777e75f7b3be554fffc2898
Guzmán Monné <guzmonne@hotmail.com>
2023-10-28 15:47:28 -0300

    fix: reverted script

--- scripts/src/git.sh
+++ scripts/src/git.sh
@@ -58,7 +58,9 @@ EOF
   c a --stream --model claude2 - <"$tmp" | tee "$response"
 
   if [[ -z "$rargs_no_git_commit" ]]; then
-    awk '/<output>/,/<\/output>/' "$response" | grep -vE '<output>|<\/output>' | perl -p -e 'chomp if eof' | git commit -F -
+    commit="$(awk '/<output>/,/<\/output>/' "$response" | grep -vE '<output>|<\/output>' | perl -p -e 'chomp if eof')"
+
+    printf '%s' "$commit" | git commit -F -
 
     git commit --amend
   fi
